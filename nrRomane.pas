d64c11d911c4e1cda3e328f167434bf8b694d213
GatmanVitalie <95142233+GatmanVitalie@users.noreply.github.com>
2024-01-28 18:49:41 +0200

    Add files via upload

---
+++ nrRomane.pas
@@ -0,0 +1,18 @@
+﻿program nrRomane;
+var
+  input: char;
+begin
+  Write('Introduceți un număr: ');
+  read(input);
+  case input of 
+  'I': Writeln(1);
+  'V': Writeln(5);
+  'X': Writeln(10);
+  'L': Writeln(50);
+  'C': Writeln(100);
+  'D': Writeln(500);
+  'M': Writeln(1000);
+  else  Writeln('număr inxistent');
+  end;
+
+end.
